dea22024bdd70edd91f7ecd388b74b181ab583dd
bharadwajponna01 <124337456+bharadwajponna01@users.noreply.github.com>
2023-02-04 14:17:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from flask_gravatar import Gravatar
 import smtplib
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = ScereteKey
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
