e4764893c1863cec3c739a5214040354620a7106
APPLE <bharadwajponna01@gmail.com>
2023-02-04 10:49:35 +0530

    small change

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///F:\Bittu\courses\python_coding\pythonProject\day-69 Blog-cap-4-adding-users/blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
